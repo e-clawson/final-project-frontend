a7e57869d8f14b26025fdf55edb7faf8e3eee945
Elizabeth Clawson <emclawson1@gmail.com>
2022-07-10 20:17:18 +0530

    text display on homepage

--- final-project-frontend/src/App.js
+++ final-project-frontend/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from './Home'
 
@@ -6,16 +5,6 @@ function App() {
   return (
     <div className="App">
       <Home />
-      {/* <Router>
-        <Navbar />
-        <Header slogan="Hello Closet!" storename="Keep track of your favorite outfits"/>
-        <Switch>
-          <Route path="/">
-            <Home />
-          </Route>
-
-        </Switch>
-      </Router> */}
     </div>
   );
 }
--- final-project-frontend/src/Home.js
+++ final-project-frontend/src/Home.js
@@ -4,19 +4,16 @@ import styled from "styled-components";
 const Home = () => {
   return (
     <div> 
-      <Wrapper>
         <h2>Homepage</h2>
-      </Wrapper>
-      
     </div>
   )
 }
 
-const Wrapper = styled.header`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 8px;
-`;
+// const Wrapper = styled.header`
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   padding: 8px;
+// `;
 
 export default Home
