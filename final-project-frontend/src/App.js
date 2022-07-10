1ac7979b13aa38549d28eec13a5f053e08715543
Elizabeth Clawson <emclawson1@gmail.com>
2022-07-10 20:52:57 +0530

    add navbar

--- final-project-frontend/src/App.js
+++ final-project-frontend/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Home from './Home'
+import Header from "./Header.js";
 
 function App() {
   return (
     <div className="App">
+      <Header slogan="Share your short stories with the world!"/>
       <Home />
     </div>
   );
