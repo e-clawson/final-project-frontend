4929382146bc68d8fbf0e2700b0c65be84d4840b
Elizabeth Clawson <emclawson1@gmail.com>
2022-07-12 16:27:27 +0530

    add usercontext and message context to app.js

--- final-project-frontend/src/index.js
+++ final-project-frontend/src/index.js
@@ -4,10 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {UserProvider} from './Context/user.js';
+import { MessageProvider } from './Context/message';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <MessageProvider>
+        <UserProvider>
+          <App />
+        </UserProvider>
+      </MessageProvider>
   </React.StrictMode>
 );
 
